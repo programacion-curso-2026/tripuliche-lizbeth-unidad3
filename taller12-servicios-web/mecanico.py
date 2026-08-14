961743cfe64eb9f4fd109bac43ecd8be2e9c22ee
Usuario Internacional <usuario@uide.edu.ec>
2026-08-13 19:15:13 -0500

    taller 12: servicios web en python

---
+++ taller12-servicios-web/mecanico.py
@@ -0,0 +1,8 @@
+class Mecanico:
+    def __init__(self,nombre,telefono,direccion,especialidad,tiempo_practica, costo_x_hora=10):
+        print("Estas llamando al Constructor")
+        self.nombre = nombre
+        self.telefono = telefono
+        self.direccion = direccion
+        self.especialidad = especialidad
+        self.tiempo_practica = tiempo_practica
